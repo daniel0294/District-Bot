8ff451dce9f2a08b4886c9fd97a38a60f9917b2f
Dan-Mac <daniel227227@yahoo.com>
2021-01-24 18:44:20 -0800

    Updated all license headers

--- src/main/java/com/districtmeps/bot/commands/normal/GitHubCommand.java
+++ src/main/java/com/districtmeps/bot/commands/normal/GitHubCommand.java
@@ -1,5 +1,5 @@
 /**
- *      Copyright 2020 Daniel Sanchez
+ *      Copyright 2020-2021 Daniel Sanchez
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
