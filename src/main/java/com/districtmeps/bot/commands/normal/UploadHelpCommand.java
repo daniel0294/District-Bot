c403318a96058e56456fb7317145fbf578774c9f
DanielGitClient <38992344+daniel0294@users.noreply.github.com>
2021-03-14 00:37:36 -0800

    -optimized embed color abit -bot downloads image and resends

--- src/main/java/com/districtmeps/bot/commands/normal/UploadHelpCommand.java
+++ src/main/java/com/districtmeps/bot/commands/normal/UploadHelpCommand.java
@@ -16,17 +16,17 @@
 
 package com.districtmeps.bot.commands.normal;
 
-import java.time.Instant;
-import java.util.List;
-
-import javax.imageio.ImageIO;
-
 import java.awt.Color;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.time.Instant;
+import java.util.List;
+
+import javax.imageio.ImageIO;
 
 import com.districtmeps.bot.objects.ICommand;
 
@@ -57,24 +57,34 @@ public class UploadHelpCommand implements ICommand {
             return;
         }
 
-        String texts = "";
-        for (String text : args) {
-            if (!checkLink(text)) {
-                event.getChannel().sendMessage("Please only send valid links").queue();
-                return;
-            }
-
-            texts += (texts + "\n");
-        }
+       if(!checkLink(args.get(0))){
+            event.getChannel().sendMessage("Please send a valid link").queue();
+            return;
+       }
 
         EmbedBuilder builder = EmbedUtils.getDefaultEmbed();
         builder.setFooter(String.format("Image posted by %#s", event.getAuthor()));
         builder.setTimestamp(Instant.now());
         builder.setDescription("[Sauce](" + args.get(0) + ")");
-        builder.setImage(att.get(0).getProxyUrl());
+        // builder.setImage(att.get(0).getProxyUrl());
+        
+        if(args.size() > 1){
+
+            String text = "";
+            
+            if(args.size() == 2){
+                text = args.get(1);
+            } else {
+
+                text = reformatListToText(args);
+            }
+
+            builder.appendDescription("\n" + text);
+        }
+        
 
 
-        BufferedImage image = null;
+        final BufferedImage image;
         try {
             URL url = new URL(att.get(0).getProxyUrl());
 
@@ -88,15 +98,35 @@ public class UploadHelpCommand implements ICommand {
             InputStream is = urlConn.getInputStream();
             image = ImageIO.read(is);
 
-            builder.setColor(averageColor(image, image.getWidth(), image.getHeight()));
+            File fileImg = new File("image.jpg");
+            ImageIO.write(image, "jpg", fileImg);
+
+            // System.out.println(Files.size(fileImg.toPath()));
+
+            builder.setImage("attachment://image.jpg");
+            // builder.setColor(averageColor(image, image.getWidth(), image.getHeight()));
+
+            // event.getChannel().sendMessage(builder.build()).queue();
+            event.getChannel().sendFile(fileImg, "image.jpg").embed(builder.build()).queue((m) -> {
+                builder.setColor(averageColor(image, image.getWidth(), image.getHeight()));
+                m.editMessage(builder.build()).queue();
+            });
+
+            fileImg.delete();
+
         } catch (IOException e) {
             logger.error("Image could not be read from link");
             builder.setColor(EmbedUtils.getDefaultColor());
+            builder.setImage(att.get(0).getProxyUrl());
+
+            event.getChannel().sendMessage(builder.build()).queue();
+        } catch(IllegalArgumentException e){
+            event.getChannel().sendMessage("File to large to send back. Max 8MiB").queue();
         }
 
         
 
-        event.getChannel().sendMessage(builder.build()).queue();
+        
 
         try{
 
@@ -107,7 +137,6 @@ public class UploadHelpCommand implements ICommand {
         
         // event.getChannel().sendMessage(att.get(0).getProxyUrl()).queue();
 
-        logger.info(texts);
 
     }
 
@@ -130,33 +159,44 @@ public class UploadHelpCommand implements ICommand {
         return link.startsWith("https://") || link.startsWith("http://");
     }
 
+    private Color averageColor(BufferedImage bi, int w, int h) {
 
-    public static Color averageColor(BufferedImage bi, int w,
-        int h) {
-    long sumr = 0, sumg = 0, sumb = 0;
-    for (int x = 0; x < w; x++) {
-        for (int y = 0; y < h; y++) {
-            Color pixel = new Color(bi.getRGB(x, y));
+        int skip = 10;
+        long sumr = 0, sumg = 0, sumb = 0;
+        for (int x = 0; x < w; x += skip) {
+            for (int y = 0; y < h; y += skip) {
+                Color pixel = new Color(bi.getRGB(x, y));
 
-            int red = pixel.getRed(), green = pixel.getGreen(), blue = pixel.getBlue();
-            
-            
-            // if((red + green + blue) < 100){
-            //     continue;
-            // }
+                int red = pixel.getRed(), green = pixel.getGreen(), blue = pixel.getBlue();
 
-            sumr += red;
-            sumg += green;
-            sumb += blue;
+                // if((red + green + blue) < 100){
+                // continue;
+                // }
+
+                sumr += red;
+                sumg += green;
+                sumb += blue;
+            }
         }
-    }
-    int num = w * h;
-    float red = sumr / num;
-    float green = sumg / num;
-    float blue = sumb / num;
-    // System.out.println(red / 255 + " - " + green / 255 + " - " + blue / 255);
+        int num = w * h;
+        num /= (skip * skip);
+        float red = sumr / num;
+        float green = sumg / num;
+        float blue = sumb / num;
+        // System.out.println(red / 255 + " - " + green / 255 + " - " + blue / 255);
 
-    return new Color(red / 255, green / 255, blue / 255);
-}
+        return new Color(red / 255, green / 255, blue / 255);
+    }
+
+    private String reformatListToText(List<String> list){
+
+        String text = "";
+
+        for (int i = 1; i < list.size(); i++) {
+            text += list.get(i) + " ";
+        }
+
+        return text;
+    }
 
 }
